1f02245c5c1b218f5b887d3ceb73dfe5ef0ebeb5
yxshbv <103039015+yxshbv@users.noreply.github.com>
2023-02-03 12:59:13 +0500

    Create t.py

---
+++ t.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def table():
+    line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+            '-' * 8,
+            '-' * 30,
+            '-' * 45,
+            '-' * 30
+        )
+    return line
